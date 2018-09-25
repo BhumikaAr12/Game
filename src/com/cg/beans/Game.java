6c034931be5f814a352d03fd12b3ace168f4b488
Arora <bhumika.arora@capgemini.com>
2018-09-25 09:18:59 +0530

    First commit

---
+++ src/com/cg/beans/Game.java
@@ -0,0 +1,21 @@
+package com.cg.beans;
+import java.util.*;
+
+public class Game {
+	private String name;
+	private byte noofplayers;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public byte getNoofplayers() {
+		return noofplayers;
+	}
+	public void setNoofplayers(byte noofplayers) {
+		this.noofplayers = noofplayers;
+	}
+
+}
---
+++ src/com/cg/repo/GameRepo.java
@@ -0,0 +1,15 @@
+package com.cg.repo;
+import java.util.*;
+
+import com.cg.beans.Game;
+
+public class GameRepo {
+	
+Game save(Game g) {
+	return null;
+}
+Game findByName(String name){
+	return null;
+}
+
+}
